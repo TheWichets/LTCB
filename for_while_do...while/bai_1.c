bfea91ecd47b2c748f37e92e1258e69776645038
Quang Vinh Cao <thewichets@gmail.com>
2025-03-06 10:11:04 +0700

    Update bai_1.c

--- for_while_do...while/bai_1.c
+++ for_while_do...while/bai_1.c
@@ -2,11 +2,12 @@
 #include <stdlib.h>
 
 int main() {
-	int i, d, sum=0;
-    for (i=0; i <10; i++){
-        scanf("%d", &d);
-        sum+=d;
-    }
-    printf("%d", sum);
-    return 0;
+    	int i, d, sum=0;
+	printf("Nhap 10 so nguyen bat ky");
+    	for (i=0; i <10; i++){
+        	scanf("%d", &d);
+        	sum+=d;
+    	}
+    	printf("Sum = %d", sum);
+    	return 0;
 }
