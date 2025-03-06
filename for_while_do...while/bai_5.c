9530daafe80f8cf77d2ac03259e324e463ae6e68
Quang Vinh Cao <thewichets@gmail.com>
2025-03-06 10:19:26 +0700

    Update bai_5.c

--- for_while_do...while/bai_5.c
+++ for_while_do...while/bai_5.c
@@ -3,7 +3,9 @@
 
 int main() {
     int i, n;
+	printf("Nhap so n:");
     scanf("%d", &n);
+	printf("Cac uoc cua %d la: ", n);
 	for (i=1; i<=n; i++){
         if (n%i==0){
             printf("%d ", i);
